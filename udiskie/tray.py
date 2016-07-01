533da9d917261b0e5eff003abb6c6d4d1f3b4bc2
Thomas Gläßle <t_glaessle@gmx.de>
2016-07-01 17:07:13 +0200

    Replace Gtk.ImageMenuItem with Gtk.Box (#57)
    as recommended here:
    https://developer.gnome.org/gtk3/stable/GtkImageMenuItem.html

--- udiskie/tray.py
+++ udiskie/tray.py
@@ -250,16 +250,15 @@ class UdiskieMenu(object):
         :returns: the menu item object
         :rtype: Gtk.MenuItem
         """
-        if icon is None:
-            item = Gtk.MenuItem()
-        else:
-            item = Gtk.ImageMenuItem()
-            item.set_image(icon)
-            # I don't really care for the "show icons only for nouns, not
-            # for verbs" policy:
-            item.set_always_show_image(True)
+        item = Gtk.MenuItem()
+        box = Gtk.Box(Gtk.Orientation.HORIZONTAL, 6)
+        # I don't really care for the "show icons only for nouns, not
+        # for verbs" policy:
+        if icon is not None:
+            box.add(icon)
         if label is not None:
-            item.set_label(label)
+            box.add(Gtk.Label(label))
+        item.add(box)
         if isinstance(onclick, Gtk.Menu):
             item.set_submenu(onclick)
         elif onclick is not None:
